c7c6329d8a27ca360e36df30d31775d4804bbc85
Tanaphatcsb64kmutnb <petchliv@gmail.com>
2024-09-13 13:29:06 +0700

    ข้อความอธิบายการเปลี่ยนแปลง

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,9 +11,17 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
+from dotenv import load_dotenv
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env_path = BASE_DIR / 'myenv'
+
+load_dotenv()
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +33,24 @@ SECRET_KEY = "django-insecure-=upq*v8z#$mjk$up-yz8cvsu^2)fs0hlclbxfxz^(sx--dilca
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+}
+
+ 
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
 
 
 # Application definition
@@ -38,7 +63,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "myapp",
-    
+    "rest_framework",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
@@ -49,6 +75,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "backend.urls"
@@ -102,6 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -123,3 +151,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWS_CREDENTIALS = True
+
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,20 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # อย่าลืม import include
+from myapp.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("myapp/user/register/", CreateUserView.as_view(), name='register'),  # as_view() ถูกต้อง
+    path("myapp/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("myapp/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("myapp-auth/", include("rest_framework.urls")),  # แก้จาก rest_framwork เป็น rest_framework
+    path("myapp/",include("myapp.urls")),
 ]
+
+
+
+
+
