f7b960a8c161949fdfe14ce570e0a1c6add1cf04
NewPhanuwit <nnewrsss@gmail.com>
2024-11-03 04:15:29 +0700

    restart

--- frontend/src/constants.js
+++
@@ -1,4 +0,0 @@
-// constants.js
-
-export const ACCESS_TOKEN = "access"; // ใช้สำหรับบันทึก access token ใน localStorage
-export const REFRESH_TOKEN = "refresh"; // ใช้สำหรับบันทึก refresh token ใน localStorage
